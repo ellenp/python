6c0fa45beb9c88b416e75e37cadfc41bd2f16699
ellenp <pates.ellen@gmail.com>
2025-05-15 23:54:10 +0800

    set

--- set.py
+++ set.py
@@ -1,4 +1,31 @@
-# duplicate entries
+# how duplicate entries behave:
 tropical_fruit_set = {"mango", "banana", "pineapple", "watermelon", "mango"}
 
 print("tropical_fruit_set: ", tropical_fruit_set)
+
+
+# use case
+
+# fast retrieval: O(1)
+
+s = set([1, 2, 2, 3])
+print(s)  # Output: {1, 2, 3}
+
+2 in s
+
+# avoid duplicate entries
+raw_emails = [
+    "alice@example.com",
+    "bob@example.com",
+    "alice@example.com",
+    "charlie@example.com",
+    "bob@example.com"
+]
+
+# Remove duplicates using a set
+unique_emails = set(raw_emails)
+
+# Now you can send one email per person
+for email in unique_emails:
+    send_newsletter(email)
+
